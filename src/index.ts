8300aad75dcf68d37210f58feaec192946af7cb0
Amon Sawamura <amotarao@gmail.com>
2025-12-20 10:34:31 +0900

    add client

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import createClientOriginal from "openapi-fetch";
+import type { components, paths } from "./schema";
+
+type CreateClientOptions = {
+  baseUrl: string;
+};
+
+export function createClient({ baseUrl }: CreateClientOptions) {
+  return createClientOriginal<paths>({
+    baseUrl,
+  });
+}
+
+export type { components };
